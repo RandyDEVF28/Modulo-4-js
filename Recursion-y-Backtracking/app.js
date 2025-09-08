2208e39dd56667dcd2f1403a471838c8fa464fb7
Randy DEVF <noiseck1796@gmail.com>
2025-09-08 00:57:29 -0600

    Creacion de la funcion para encontrar regalo

--- Recursion-y-Backtracking/app.js
+++ Recursion-y-Backtracking/app.js
@@ -1,2 +1,18 @@
 // Lista de regalos
 const gifts = ["Muñeca", "Carro de juguete", "Rompecabezas", "Lego", "Pelota"];
+
+
+function findGift(gifts, giftName, index = 0) {
+    
+    if (index === gifts.length) {
+        return `${giftName} no está en la lista.`;
+    }
+
+    
+    if (gifts[index] === giftName) {
+        return `${giftName} está en la posición ${index}.`;
+    }
+
+    
+    return findGift(gifts, giftName, index + 1);
+}
