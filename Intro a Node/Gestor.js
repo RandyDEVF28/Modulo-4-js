ab4292a9379c5baa4cf20a6f4895eac6665ab674
Randy DEVF <noiseck1796@gmail.com>
2025-09-08 01:22:08 -0600

    Funcion para listar notas y funcion eliminar notas

--- Intro a Node/Gestor.js
+++ Intro a Node/Gestor.js
@@ -17,3 +17,26 @@ function agregarNota(titulo, contenido) {
   fs.writeFileSync(filePath, JSON.stringify(notas, null, 2));
   console.log('Nota agregada con exito.');
 }
+
+function listarNotas() {
+  if (fs.existsSync(filePath)) {
+    const data = fs.readFileSync(filePath, 'utf8');
+    const notas = JSON.parse(data);
+    console.log(notas);
+  } else {
+    console.log('No hay notas guardadas.');
+  }
+}
+
+function eliminarNota(titulo) {
+  if (fs.existsSync(filePath)) {
+    const data = fs.readFileSync(filePath, 'utf8');
+    let notas = JSON.parse(data);
+    const notasRestantes = notas.filter(nota => nota.titulo !== titulo);
+
+    fs.writeFileSync(filePath, JSON.stringify(notasRestantes, null, 2));
+    console.log(`Nota con título "${titulo}" eliminada.`);
+  } else {
+    console.log('No hay notas para eliminar.');
+  }
+}
